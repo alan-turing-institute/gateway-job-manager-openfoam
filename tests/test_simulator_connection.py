2c706c70739aad72ad5a590a0f40129e24f7b23a
nbarlowATI <nbarlow@turing.ac.uk>
2018-04-10 11:54:39 +0100

    start to update jobmanager tests

--- tests/test_simulator_connection.py
+++ tests/test_simulator_connection.py
@@ -2,10 +2,18 @@
 Test the simulator connection.
 """
 
-from connection.simulator import Connection
+from config import DevelopmentConfig
+
+from connection.simulator import SSH_Credentials,SimulatorConnection
+
+from .fixtures import demo_app 
 
 def test_simulator_connection():
-    connection = Connection()
+    app = demo_app()
+    devconfig = DevelopmentConfig()
+    app.config.from_object(devconfig)
+    credentials = SSH_Credentials(app.config)
+    connection = SimulatorConnection(credentials)
     # out, err, exit_code = connection._run_remote_command('echo hello')
     # print(out, err, exit_code)
     # print(connection.hostname)
