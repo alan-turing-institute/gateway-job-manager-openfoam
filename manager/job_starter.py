cffd02d84cebafb132b6650aacd08a67d22edabd
masonlr <masonlr@unimelb.edu.au>
2018-06-15 16:23:32 +0100

    Assume all scripts are called from simulation root

--- manager/job_starter.py
+++ manager/job_starter.py
@@ -9,26 +9,26 @@ from flask import current_app
 from preprocessor import file_getter, patcher, file_putter
 import tempfile
 
-    
+
 def preprocess(scripts, parameters, job_id):
     """
     get scripts from remote storage to a local disk (keeping the same filename)
     patch them (changing filename), and copy to final location.
     """
-    
+
     tmp_dir = current_app.config["LOCAL_TMP_DIR"]
     job_dir = os.path.join(tmp_dir, str(job_id))
-    
+
     # make some local directories for the raw and patched scripts
     job_dir_raw = os.path.join(job_dir, 'raw')
     os.makedirs(job_dir_raw, exist_ok=True)
-    job_dir_patch = os.path.join(job_dir, 'patched') 
+    job_dir_patch = os.path.join(job_dir, 'patched')
     os.makedirs(job_dir_patch, exist_ok=True)
-    
+
     # get the scripts from cloud storage
     fetched_ok, message = file_getter.get_remote_scripts(scripts,
                                                          job_dir_raw)
-    if not fetched_ok:        
+    if not fetched_ok:
         return 'problem fetching scripts: {}'.format(message), -1
     # patch the scripts using Mako
     patched_ok, message = patcher.patch_all_scripts(scripts,
@@ -41,7 +41,7 @@ def preprocess(scripts, parameters, job_id):
     destination_dir = current_app.config["SIM_TMP_DIR"]
     copied_ok, message = file_putter.copy_scripts_to_backend(job_dir_patch,
                                                              destination_dir,
-                                                             job_id) 
+                                                             job_id)
     if not copied_ok:
         return 'problem copying files: {}'.format(message), -1
     return 'preprocessing succeeded', 0
@@ -54,17 +54,24 @@ def execute_action(scripts, job_id, action):
     """
     sim_connection = file_putter.get_simulator_connection()
     message, status = 'No actions executed yet', 0
+
+    sim_root = current_app.config["SIM_TMP_DIR"]
+
     for script in scripts:
-        script_location = os.path.join(current_app.config["SIM_TMP_DIR"],
+        script_path = os.path.join(sim_root,
                                        str(job_id),
                                        script["destination"])
-        script_dir = os.path.dirname(script_location)
-        script_name = os.path.basename(script_location)
+
+        script_dirname = os.path.dirname(script_path)
+        script_name = os.path.basename(script_path)
+
         if script["action"] == action:
             if action == "RUN":
-                run_cmd = 'cd '+script_dir+'; bash ./'+script_name
+                run_cmd = 'cd {} && bash ./{} >& log.{}'.format(
+                    sim_root, script_name, script_name
+                )
                 out, err, status = sim_connection.run_remote_command(
-                    run_cmd+' >& /tmp/output.txt'
+                    run_cmd
                 )
                 message = 'stdout: {}\n stderr: {}'.format(out,err)
                 break
